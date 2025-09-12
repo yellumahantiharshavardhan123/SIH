19b8a8db84af705cc3a5dd7a6213ed15c97084ae
yellumahantiharshavardhan123 <yellumahantiharshavardhan@gmail.com>
2025-09-12 14:48:49 +0000

    Sandbox snapshot commit

--- yellumahantiharshavardhan123/SIH/apps/mobile/lib/config/maptiler.dart
+++ yellumahantiharshavardhan123/SIH/apps/mobile/lib/config/maptiler.dart
@@ -1,4 +1,4 @@
 class MapTilerConfig {
-  static const key = String.fromEnvironment('MAPTILER_KEY', defaultValue: 'YOUR_KEY');
+  static const key = String.fromEnvironment('MAPTILER_KEY', defaultValue: 'GgOZjiIxfo4NTtzvhpud');
   static const styleUrl = 'https://api.maptiler.com/maps/streets/style.json?key=' + key;
 }
